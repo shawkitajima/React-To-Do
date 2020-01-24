549b85d6c83859171a2e14973cd5ebaa940a2980
Shaw Kitajima <shawkitajima@gmail.com>
2020-01-24 15:45:51 -0800

    makes todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [todos, setTodos] = useState([]);
+
+  let [newTodo, setNewTodo] = useState({
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='whole'>
+      <h1>A Simple React To Do Application</h1>
+      <div className='submit'>
+        <input onChange={e => setNewTodo({todo: e.target.value, status: 'Incomplete'})}></input>
+        <button onClick={() => setTodos([...todos, newTodo])}>Add Todo</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Todo</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+      {todos.map((todo, idx) => (
+        <tr key={idx}>
+          <td>{todo.todo}</td>
+          <td>{todo.status}</td>
+          {todo.status === 'Incomplete' ? 
+          (<td><button onClick={idx, evt => {
+              let todosCopy = [...todos];
+              let obj = {...todosCopy[idx]}
+              obj.status = 'Complete';
+              todosCopy.splice(idx, 1, obj);
+              setTodos(todosCopy)
+            }}>Complete</button></td>) 
+            :
+            (<td><button onClick={idx, evt => {
+              let todosCopy = [...todos];
+              todosCopy.splice(idx, 1);
+              setTodos(todosCopy)
+            }}>Delete</button></td>)
+            }
+        </tr>
+      ))}
+        </tbody>
+      </table>
     </div>
-  );
+    )    
 }
 
 export default App;
